240a65ab28b6a60a89637ac1494c273e23eeb0b6
Kens04 <68502805+Kens04@users.noreply.github.com>
2025-01-04 11:27:49 +0900

    ブログ一覧作成

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,9 +1,39 @@
-const MainPage = () => {
-  return (
-    <div>
-      <div>MainPage</div>
-    </div>
-  )
-}
+import { createClient } from "@/utils/supabase/server";
+import { Suspense } from "react";
+import Loading from "@/app/loading";
+import BlogItem from "@/components/blog/BlogItem";
 
-export default MainPage
+// メインページ
+const MainPage = async () => {
+  const supabase = await createClient();
+
+  // ブログ一覧取得
+  const { data: blogsData, error } = await supabase
+    .from("blogs")
+    .select(
+      `
+      *,
+      profiles (
+        name,
+        avatar_url
+      )
+    `
+    )
+    .order("updated_at", { ascending: false });
+
+  if (!blogsData || error) {
+    return <div className="text-center">ブログが投稿されていません</div>;
+  }
+
+  return (
+    <Suspense fallback={<Loading />}>
+      <div className="grid grid-cols-3 gap-5">
+        {blogsData.map((blog) => {
+          return <BlogItem key={blog.id} blog={blog} />;
+        })}
+      </div>
+    </Suspense>
+  );
+};
+
+export default MainPage;
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,23 @@
 export interface ProfileType {
-  id: string
-  name: string
-  introduce: string | null
-  avatar_url: string | null
+  id: string;
+  name: string;
+  introduce: string | null;
+  avatar_url: string | null;
+}
+
+export interface ProfileType {
+  id: string;
+  name: string;
+  introduce: string | null;
+  avatar_url: string | null;
+}
+
+export interface BlogType {
+  id: string;
+  title: string;
+  content: string;
+  user_id: string;
+  image_url: string | null;
+  updated_at: string;
+  created_at: string;
 }
